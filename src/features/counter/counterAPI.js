45c0b698295b836b554d66c1df4f4d659766064d
maen <maenkh@yahoo.com>
2022-07-25 17:40:47 +0300

    fetch data from API to console

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,4 +1,11 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) => setTimeout(() => resolve({ data: amount }), 500));
+export function fetchCount() {
+  console.log('fetching data');
+  let url = 'https://newsdata.io/api/1/news?apikey=pub_95677350f565275afd7e8baa3ba52ce1533e';
+  let req = new Request(url);
+  fetch(req)
+  .then(function(response) {
+      return response.json();
+  })
+  .then(data => console.log(data));
 }
