04f8fd6417d148a9cfcdb02c0ba431a7a1b53608
maen <maenkh@yahoo.com>
2022-07-28 01:11:37 +0300

    finish search function

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import { selectState } from '../features/COVID/COVID-Slice';
@@ -6,35 +6,51 @@ import { selectState } from '../features/COVID/COVID-Slice';
 import '../style.css';
 
 export default function Countries() {
+  // get the country list depend on the pass continent name
   const countries = useSelector(selectState);
   const location = useLocation();
   const { name } = location.state;
-
   const countriesList = countries.continents.find(
     (country) => country.name === name,
   );
-  console.log(countriesList);
+  // ********************************* //
+
+  const [filter, setFilter] = useState('');
+  const [foundCountry, setFoundCountry] = useState(countriesList.countries);
+
+  const filterFunc = (e) => {
+    const keyword = e.target.value;
+    if (keyword !== '') {
+      const result = countriesList.countries
+        .filter((country) => country.Country.toLowerCase().startsWith(keyword.toLowerCase()));
+      setFoundCountry(result);
+    } else {
+      setFoundCountry(countriesList.countries);
+    }
+    setFilter(keyword);
+  };
+
   return (
     <div>
       <h1>countries list</h1>
       <Link to="/">Home</Link>
+      <input
+        type="search"
+        placeholder="search"
+        id="filter"
+        value={filter}
+        onChange={filterFunc}
+      />
       <div>
-        {countriesList.countries.map((country) => (
-          <li key={country.id}>
-            <p>{country.Country}</p>
-            <p>{country.TotalCases}</p>
-          </li>
+        {foundCountry?.map((country) => (
+          <ul key={country?.id}>
+            <li>
+              <p>{country?.Country}</p>
+              <p>{country?.TotalCases}</p>
+            </li>
+          </ul>
         ))}
       </div>
     </div>
   );
 }
-
-// { {countries.continents.countries.map((item) => (
-//     <div key={item.id}>
-//       <div>
-//         <p>{item?.Country}</p>
-//         {/* <p>{item.TotalCases}</p> */}
-//       </div>
-//     </div>
-//   ))} }
