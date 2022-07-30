8ccd0797da6ef1f914e49568b6b5f0131acad09c
maen <maenkh@yahoo.com>
2022-07-30 14:06:04 +0300

    fix linter

--- src/features/COVID/COVID-Slice.js
+++ src/features/COVID/COVID-Slice.js
@@ -25,7 +25,6 @@ export const fetchContinentsAsync = createAsyncThunk(
       totalCases: totalcases,
       countries: response,
     };
-    console.log(data);
     return data;
   },
 );
--- src/features/COVID/__test__/COVID-Slice.test.js
+++ src/features/COVID/__test__/COVID-Slice.test.js
@@ -1,28 +1,18 @@
-import reducer, {
-    initialState,
-    fetchContinentsAsync
-  } from "../COVID-Slice";
+import { fetchContinentsAsync } from '../COVID-Slice';
 
-  import store from '../../../App/store';
-  
- /**
-   * Testing the createUser thunk
-   */
+import store from '../../../App/store';
 
-  describe("Fetch continents list", () => {
-    // beforeAll(() => {
-    //   mockNetWorkResponse();
-    // });
+/**
+ * Testing the reducer thunk
+ */
 
-    it("Should be able to fetch a continents list", async () => {
+describe('Fetch continents list', () => {
+  it('Should be able to fetch a continents list', async () => {
+    const result = await store.dispatch(fetchContinentsAsync('asia'));
 
+    const countries = result.payload.totalCases;
+    const state = store.getState().COVID.continents[1].totalCases;
 
-      const result = await store.dispatch(fetchContinentsAsync('asia'));
-
-      const countries = result.payload.totalCases;     
-      const state = store.getState().COVID.continents[1].totalCases;
-
-
-      expect(countries).toEqual(state);     
-    });
-})
+    expect(countries).toEqual(state);
+  });
+});
